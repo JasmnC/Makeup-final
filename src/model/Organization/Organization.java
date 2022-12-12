db3468ba5feef0b44154d19cdd9bee7e225e169d
JasmnC <chen.jasm@northeastern.edu>
2022-12-11 19:10:48 -0500

    model.organization

--- src/model/Organization/Organization.java
+++ src/model/Organization/Organization.java
@@ -4,10 +4,90 @@
  */
 package model.Organization;
 
+import java.util.ArrayList;
+import model.Role.Role;
+
 /**
  *
  * @author yuujadz
  */
-public class Organization {
+public abstract class Organization {
+    
+    private Type type;
+    private String name;
+//    private WorkQueue workQueue;
+//    private UserAccountDirectory userAccountDirectory;
+    private int organizationID;
+    private static int idCounter;
+    
+    public enum Type {
+        
+        WelcomingOrg("Welcoming Organization"), 
+        MemberOrg("Member Organization"), 
+        PhisicalCheckOrg("Physical Check Organization"),
+        ManagementOrg("Managment Organization"),
+        PersonaTrainerOrg("Personal Trainer Organization"), 
+        FinanceOrg("Finance Organization");
+        
+        private String value;
+        
+        private Type(String value) {
+            this.value = value;
+        }
+        
+        public String getValue() {
+            return value;
+        }
+        
+     @Override
+        public String toString() {
+            return value;
+        }
+        
+    }
+
+    public Organization(String name, Type type) {
+        this.name = name;
+        this.type = type;
+//        workQueue = new WorkQueue();
+//        userAccountDirectory = new UserAccountDirectory();
+        organizationID = idCounter;
+        ++idCounter;
+    }
+
+    public abstract ArrayList<Role> getSupportedRole();
+    
+//    public UserAccountDirectory getUserAccountDirectory() {
+//        return userAccountDirectory;
+//    }
+
+    public int getOrganizationID() {
+        return organizationID;
+    }
+
+    public Type getType() {
+        return type;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+//    public WorkQueue getWorkQueue() {
+//        return workQueue;
+//    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public void setWorkQueue(WorkQueue workQueue) {
+//        this.workQueue = workQueue;
+//    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
     
 }
--- src/model/Organization/OrganizationDirectory.java
+++ src/model/Organization/OrganizationDirectory.java
@@ -4,10 +4,68 @@
  */
 package model.Organization;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author yuujadz
  */
 public class OrganizationDirectory {
+        private ArrayList<Organization> organizationList;
+
+    public OrganizationDirectory() {
+        organizationList = new ArrayList();
+    }
+
+    public ArrayList<Organization> getOrganizationList() {
+        return organizationList;
+    }
     
+    public Organization createOrganization(String name, Organization.Type type){
+        Organization organization = null;
+        if (type.getValue().equals(Organization.Type.WelcomingOrg.getValue())){
+            organization = new WelcomingOrganization(name);
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Organization.Type.MemberOrg.getValue())){
+            organization = new MemberOrganization(name);
+            organizationList.add(organization);
+        }
+         else if (type.getValue().equals(Organization.Type.PhisicalCheckOrg.getValue())){
+            organization = new PhisicalCheckOrganization(name);
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Organization.Type.ManagementOrg.getValue())){
+            organization = new ManagementOrganization(name);
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Organization.Type.PersonaTrainerOrg.getValue())){
+            organization = new PersonaTrainerOrganization(name);
+            organizationList.add(organization);
+        }
+       
+        else if (type.getValue().equals(Organization.Type.FinanceOrg.getValue())){
+            organization = new FinanceOrganization(name);
+            organizationList.add(organization);
+        }
+        
+        return organization;
+    
+}
+    
+     public boolean nameIsUnique(String name){
+        for(Organization organization : organizationList){
+            if(name.equalsIgnoreCase(organization.getName())){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public Organization getOrganizationByName(String name){
+        for (Organization o : organizationList){
+            if (name.equalsIgnoreCase(o.getName())) return o;
+        }
+        return null;
+    } 
 }
